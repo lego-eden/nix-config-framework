4d3f2d5697250b0317c9102fc6d02a648d635750
Hampus Edén <hampus.eden@gmail.com>
2026-03-09 18:59:21 +0100

    add gnome and kde desktops

--- configuration.nix
+++ configuration.nix
@@ -185,6 +185,25 @@
     enableSSHSupport = true;
   };
 
+  specialisation.kde-test.configuration = {
+    services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      settings.General.DisplayServer = "wayland";
+    };
+    services.desktopManager.plasma6.enable = true;
+  };
+
+  specialisation.gnome-test.configuration = {
+    services.displayManager.gdm.enable = true;
+    services.desktopManager.gnome.enable = true;
+
+    # To disable installing GNOME's suite of applications
+    # and only be left with GNOME shell.
+    services.gnome.core-apps.enable = true;
+    services.gnome.core-developer-tools.enable = true;
+    services.gnome.games.enable = true;
+  };
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
